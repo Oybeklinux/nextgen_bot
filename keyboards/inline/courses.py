59515c391151795672d7014ed8153db201996ca7
Oybek <Oybeklinux@gmail.com>
2023-11-13 17:56:01 +0500

    bug fix: error occuring when user selects course

--- keyboards/inline/courses.py
+++ keyboards/inline/courses.py
@@ -13,14 +13,16 @@ async def get_general_ikb(course):
             [
                 # InlineKeyboardButton(text=Texts().get("iopen_lesson"),
                 #                   callback_data=register_ol_cb.new(course=course, submenu='register_to_open_lesson')),
-            InlineKeyboardButton(text=await _("iopen_lesson_vip"),
-                                 callback_data=general_cb.new(course=course, submenu='register_to_open_lesson_vip', is_menu=None)),
-             InlineKeyboardButton(text=await _("iget_in_touch"),
-                                  url='https://t.me/kamilaa3')],
+                InlineKeyboardButton(text=await _("iopen_lesson_vip"),
+                                     callback_data=general_cb.new(course=course, submenu='register_to_open_lesson_vip',
+                                                                  is_menu='')),
+                InlineKeyboardButton(text=await _("iget_in_touch"),
+                                     url='https://t.me/kamilaa3')],
             [InlineKeyboardButton(text=await _("icourse_content"),
-                                  callback_data=general_cb.new(submenu="course_content", course=course, is_menu=None)),
+                                  callback_data=general_cb.new(submenu="course_content", course=course, is_menu='')),
              InlineKeyboardButton(text=await _("icourse_objective"),
-                                  callback_data=general_cb.new(submenu="course_objective", course=course, is_menu=None))],
+                                  callback_data=general_cb.new(submenu="course_objective", course=course,
+                                                               is_menu=''))],
             [InlineKeyboardButton(text=await _("bto_main"),
                                   callback_data=back_to_course_cb.new(value='back'))]
         ]
@@ -33,9 +35,9 @@ confirm_ol_cb = CallbackData("confirm_ol", 'submenu')
 async def get_confirm_ol_ikb(course):
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text=await _("iconfirm"),callback_data=confirm_ol_cb.new(submenu=course)),
-            InlineKeyboardButton(text=await _("icancel"), callback_data=confirm_ol_cb.new(submenu='cancel'))
-            ]
+            [InlineKeyboardButton(text=await _("iconfirm"), callback_data=confirm_ol_cb.new(submenu=course)),
+             InlineKeyboardButton(text=await _("icancel"), callback_data=confirm_ol_cb.new(submenu='cancel'))
+             ]
         ]
     )
 
