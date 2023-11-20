d5d5e3a7a3e5ec45823a1f63b5058aa4351adb8a
Oybek <Oybeklinux@gmail.com>
2023-11-20 16:41:15 +0500

    change: show participants

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,6 +2,7 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 from aiogram.types import ParseMode
 
+from data.config import admins
 from data.texts import _
 from loader import dp
 from utils.misc import rate_limit
@@ -11,6 +12,9 @@ from utils.misc import rate_limit
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = await _('commands')
+    if message.from_user and message.from_user.id in admins:
+        text.append("/today - bugun ro'yxatdan o'tganlar")
+        text.append("/sendsms - hammaga habar jo'natish")
 
     await message.answer('\n'.join(text), parse_mode=ParseMode.MARKDOWN)
 
--- handlers/users/statistics.py
+++ handlers/users/statistics.py
@@ -12,8 +12,9 @@ async def send_sms_command(message: types.Message):
     users = await db.select_users_by_date(date.today())
 
     text = f"Bugun ro'yxatdan o'tganlar ({len(users)}):"
+    text += f"\n{'ID'.center(12)} | {'name'.center(14)} | 🏆 "
     for user in users:
         if user['name'] and user['phone']:
             is_winner = '🏆' if user['is_winner'] else ''
-            text += f"\n{str(user['id']).center(12)} | {user['name'][:12].center(14)} | {is_winner}"
+            text += f"\n{str(user['id']).center(12)} | {user['name'][:12].center(14)} | {is_winner} "
     await message.answer(text=f'<code>{text}</code>')
